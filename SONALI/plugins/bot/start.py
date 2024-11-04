5305735c2c91196d7a294640f23d28cc9ca95cb1
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-11-05 04:28:49 +0530

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
     typing_message = await message.reply("<b>𝖣ɪɴɢ..𝖣ᴏɴɢ..❤️‍🔥</b>")  # Initial message
     
     # Simulate typing
-    typing_text = "<b>𝖲ᴛᴀʀᴛɪɴɢ...❤️‍🔥</b>"
+    typing_text = "<b>𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐃𝐨𝐥𝐥 𝐌𝐮𝐬𝐢𝐜..🥰</b>"
     
     for i in range(1, len(typing_text) + 1):  # Loop through each character
         try:
